5320e5a4767b9f52e0a95ef66045d8149b55e143
Vladislav Smirnov <mr.bikus@yandex.ru>
2021-06-08 00:41:30 +0200

    refactoring ♻️ added css variables 💄 added home page 💥

---
+++ src/constants/pages.ts
@@ -0,0 +1,6 @@
+export const PAGES = {
+    HOME: 'home',
+    QUESTIONS: 'questions',
+    NEW: 'new',
+    ABANDONED: 'abandoned',
+};
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,17 +1,12 @@
 import React, { useEffect } from 'react';
-import s from 'styled-components';
 import { List } from 'components/list';
 import { useStores } from 'store';
 import { Typography } from 'components/typography';
 
-type Props = {};
-
-const StyledHome = s.div``;
-
 /**
  * Home Component
  */
-export const Home: React.FC<Props> = () => {
+export const Home: React.FC = () => {
     const { pollsStore } = useStores();
 
     useEffect(() => {
