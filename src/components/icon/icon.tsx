67b52e677db7150bc66a9bcd22e5f0d7944d6a10
Vladislav Smirnov <mr.bikus@yandex.ru>
2021-06-08 00:30:52 +0200

    created Icon component ✨ added two icons 🍱

---
+++ src/components/icon/icon.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import s from 'styled-components';
+
+import * as commonIcons from './svg/common/index';
+
+interface Icons {
+    [key: string]: any;
+}
+
+interface Sizes {
+    [name: string]: number | string;
+}
+
+const icons: Icons = {
+    ...commonIcons,
+};
+
+type Props = {
+    /**
+     * Icon name.
+     */
+    name?: string;
+    /**
+     * Icon size
+     */
+    size?: 'responsive' | 'xsmall' | 'small' | 'medium' | 'large';
+    color?: string;
+    className?: string;
+};
+
+const SIZE_RESPONSIVE = 'responsive';
+const SIZE_EXTRA_SMALL = 'xsmall';
+const SIZE_SMALL = 'small';
+const SIZE_MEDIUM = 'medium';
+const SIZE_LARGE = 'large';
+
+/* Sizes values */
+const SIZES: Sizes = {
+    [SIZE_EXTRA_SMALL]: 13,
+    [SIZE_SMALL]: 17,
+    [SIZE_MEDIUM]: 22,
+    [SIZE_LARGE]: 34,
+    [SIZE_RESPONSIVE]: '100%',
+};
+
+/**
+ * Icon component
+ */
+const BaseIcon: React.FC<Props> = ({
+    name = '',
+    size = SIZE_RESPONSIVE,
+    className,
+}) => {
+    const sizeValue = SIZES[size] || size;
+    const IconComponent = icons[name];
+
+    if (typeof IconComponent === 'undefined') {
+        console.error('Requested icon does not exist');
+        return null;
+    }
+
+    return (
+        <IconComponent
+            width={sizeValue}
+            height={sizeValue}
+            className={className}
+        />
+    );
+};
+
+export const Icon: React.FC<Props> = s(BaseIcon)`
+    fill: var(--color-${(props: Props) =>
+        props.color || 'dark-medium'});            
+`;
---
+++ src/components/icon/svg/common/index.ts
@@ -0,0 +1,4 @@
+import { ReactComponent as check } from './check.svg';
+import { ReactComponent as poll } from './poll.svg';
+
+export { check, poll };
